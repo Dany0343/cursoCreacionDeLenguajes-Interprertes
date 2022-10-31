b5a4095243059cec2e4a2ef8dbd6dbec1c6ef056
Oscar Bucio <oscarbucio2001@gmail.com>
2022-10-31 14:38:35 -0600

    add: ast.py first node and 2 next. Statement node and Expression node

---
+++ lpp/ast.py
@@ -0,0 +1,35 @@
+from abc import ( # abc es abstract base class
+    ABC,
+    abstractmethod,
+)
+
+from lpp.token import Token
+
+
+class ASTNode(ABC):
+    @abstractmethod # Se utiliza la funcion como decorador para señalar que es un método abstracto
+    def token_literal(self) -> str: # Se usa principalmente para generar debugging y ver que todo funciona dentro de los nodos
+        pass # Si se trata de inicializar ASTNode daría un error, si se intenta inicializar una subclase que no implementa uno de estos métodos tambien daría error el interprete de Python
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+# Estos 2 nodos de abajo no se van a inicializar jamás, se podría pero no se hará. Ya que solo se van a utilizar como referencias de tipos
+
+class Statement(ASTNode): 
+    def __init__(self, token: Token) -> None: # Constructor que recibe un token, no regresa nada y lo unico que hace es inicializar este token en una variable llamada token
+        self.token = token
+
+    def token_literal(self) -> str: # Se tienen que implementar los métodos del padre
+        return self.token.literal # Simplemente el pedazo de string del programa que está en ese token
+
+    # No se va a implementar el dunder __str__ dentro de Statement, lo cual provoca que no se pueda inicializar porque habría un error de ASTNode porque la clase abstracta dice que tenemos que implementarlo, pero no hay problema porque nunca se va a inicializar Statement de manera directa
+
+
+class Expression(ASTNode):
+    def __init__(self, token: Token) -> None:
+        self.token = token
+
+    def token_literal(self) -> str:
+        return self.token.literal
